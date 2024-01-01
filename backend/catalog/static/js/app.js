c69e89439c49be5156e5737006f070fb9222df11
JOSE EDUARDO SANTOS LONDE <edulonde@gmail.com>
2024-01-01 17:04:55 -0300

    add: estilização e efeitos front-end

---
+++ backend/catalog/static/js/app.js
@@ -0,0 +1,9 @@
+    const app = Vue.createApp({
+        data() {
+            return {
+                showMenu: true,
+                showDropdown: false
+            }
+        }
+    })
+    app.mount('#app')
