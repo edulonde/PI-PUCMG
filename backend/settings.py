98e5922c2d68a8c89585e6a597194f2dc7b6674b
JOSE EDUARDO SANTOS LONDE <edulonde@gmail.com>
2024-01-13 21:30:34 -0300

    add allowed hosts

--- backend/settings.py
+++ backend/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['beeblius-128f006ec4d3.herokuapp.com']
 
 # Application definition
 
