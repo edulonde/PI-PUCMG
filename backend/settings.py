50f18b8fb2de73d122117d0aff1b4b6c0e8c6932
JOSE EDUARDO SANTOS LONDE <edulonde@gmail.com>
2024-01-14 07:40:08 -0300

    config database postgres

--- backend/settings.py
+++ backend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dotenv
+import dj_database_url
 # import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -87,10 +88,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+        ssl_require=True,
+    ),
 }
 
 # Password validation
